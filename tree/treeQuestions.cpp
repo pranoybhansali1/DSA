785ef3bb8f08e98d82633193c6fbe887bf09325f
Pranoy Bhansali <54235683+pranoybhansali1@users.noreply.github.com>
2022-08-31 09:30:55 +0530

    Update treeQuestions.cpp

--- tree/treeQuestions.cpp
+++ tree/treeQuestions.cpp
@@ -136,3 +136,18 @@ int getSize(Node* root)
         return 0;
     return getSize(root->left) + 1 + getSize(root->right);
 }
+
+// Max and min element in Binary Tree
+// https://practice.geeksforgeeks.org/problems/max-and-min-element-in-binary-tree/1
+int findMax(Node *root)
+{
+		if(!root)
+				return INT_MIN;
+		return max(root->data, max(findMax(root->left), findMax(root->right)));
+}
+int findMin(Node *root)
+{
+		if(!root)
+				return INT_MAX;
+		return min(root->data, min(findMin(root->left), findMin(root->right)));
+}
